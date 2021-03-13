67e22280c0ffc69e7935c1abdc8f0b3d2f9ce7f2
vasilyook <62177740+vasilyook@users.noreply.github.com>
2021-03-13 13:36:00 +0300

    Create Robot-correct.py

---
+++ back/Robot-correct.py
@@ -0,0 +1,19 @@
+class Robot(object):
+    moves = {"N": [0, 1], "S": [0, -1], "E": [1, 0], "W": [-1, 0]}
+    lastMoves = []
+
+    def __init__(self, coors):
+        self.x = coors[0]
+        self.y = coors[1]
+        self.lastMoves = [(self.x, self.y)]
+
+    def move(self, stringCommands):
+        self.lastMoves = [(self.x, self.y)]
+        for l in stringCommands:
+            self.x += self.moves[l][0]
+            self.y += self.moves[l][1]
+            self.lastMoves.append((self.x, self.y))
+        return (self.x, self.y)
+
+    def path(self):
+        return self.lastMoves
